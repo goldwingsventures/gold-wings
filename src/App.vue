1ece0afc74aac782846be2f848dfde3d146210a3
Mahender Kumar <mahender@tsp.dev>
2023-10-06 10:47:49 +0530

    GW First Push

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 
 <script setup>
-import SampleComponent from './components/SampleComponent.vue'
+import SampleComponent from './components/HeaderNav.vue'
+import MainBody from "@/components/MainBody.vue";
 </script>
 <template>
 <SampleComponent/>
+<MainBody/>
 </template>
 
 <style src="./assets/tailwind.css"/>
---
+++ src/components/MainBody.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+<style>
+@media (max-width: 1024px) {
+  .text-9xl {
+    font-size: 3.5rem;
+  }
+
+  .text-8xl {
+    font-size: 3.5rem;
+  }
+  .max-w-200 {
+    max-width: 150px;
+  }
+}
+</style>
+<template>
+  <main class="relative bg-black min-h-screen flex justify-center items-center backdrop-blur-3xl" >
+    <div class="flex flex-col align-middle items-center justify-center">
+      <div class="text-white text-9xl text-center mb-8">
+        <h1>Coming Soon...</h1>
+      </div>
+      <div class="max-w-200 mx-auto">
+        <img src="/logo.png" alt="Your Logo" />
+      </div>
+      <div class="text-amber-200 my-4 text-8xl text-center mt-4">
+        <h1>Gold Wings Ventures</h1>
+      </div>
+    </div>
+  </main>
+</template>
+  
