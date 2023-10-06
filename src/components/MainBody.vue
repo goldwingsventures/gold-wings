9577dba848bfe14766eaf6e4711b60615d099a51
Mahender Kumar <mahender@tsp.dev>
2023-10-06 11:26:49 +0530

    GW First Push

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -17,14 +17,14 @@
 </style>
 <template>
   <main class="relative bg-black min-h-screen flex justify-center items-center backdrop-blur-3xl" >
-    <div class="flex flex-col align-middle items-center justify-center">
-      <div class="text-white text-9xl text-center mb-8" style="max-width: 100%; height: auto;">
+    <div class="flex flex-col align-middle items-center justify-center" style="max-width: fit-content ; height: auto;">
+      <div class="text-white text-9xl text-center mb-8">
         <h1>Coming Soon...</h1>
       </div>
       <div class="max-w-200 mx-auto">
-        <img src="/logo.png" alt="Your Logo" style="max-width: 100%; height: auto;"/>
+        <img src="/logo.png" alt="Your Logo"/>
       </div>
-      <div class="text-amber-200 my-4 text-8xl text-center mt-4" style="max-width: 100%; height: auto;">
+      <div class="text-amber-200 my-4 text-8xl text-center mt-4">
         <h1>Gold Wings Ventures</h1>
       </div>
     </div>
