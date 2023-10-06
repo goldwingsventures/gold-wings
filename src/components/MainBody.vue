98bee4dfd0f036952881025b8262f66d263084f0
Mahender Kumar <mahender@tsp.dev>
2023-10-06 11:16:29 +0530

    GW First Push

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -4,11 +4,11 @@
 <style>
 @media (max-width: 1024px) {
   .text-9xl {
-    font-size: 3.5rem;
+    font-size: 3rem;
   }
 
   .text-8xl {
-    font-size: 3.5rem;
+    font-size: 3rem;
   }
   .max-w-200 {
     max-width: 150px;
@@ -18,13 +18,13 @@
 <template>
   <main class="relative bg-black min-h-screen flex justify-center items-center backdrop-blur-3xl" >
     <div class="flex flex-col align-middle items-center justify-center">
-      <div class="text-white text-9xl text-center mb-8">
+      <div class="text-white text-9xl text-center mb-8" style="max-width: 100%; height: auto;">
         <h1>Coming Soon...</h1>
       </div>
       <div class="max-w-200 mx-auto">
-        <img src="/logo.png" alt="Your Logo" />
+        <img src="/logo.png" alt="Your Logo" style="max-width: 100%; height: auto;"/>
       </div>
-      <div class="text-amber-200 my-4 text-8xl text-center mt-4">
+      <div class="text-amber-200 my-4 text-8xl text-center mt-4" style="max-width: 100%; height: auto;">
         <h1>Gold Wings Ventures</h1>
       </div>
     </div>
